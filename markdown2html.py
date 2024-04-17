b92ef1b288805f84a964341b7809bbe06418d238
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-04-17 13:40:01 +0200

    paragraph attempt

--- markdown2html.py
+++ markdown2html.py
@@ -7,15 +7,13 @@ Write a script markdown2html.py that takes two string arguments:
 """
 import sys
 import os
-import re
-import hashlib
 
-def heading(line):
+def heading(line, in_heading=False):
     ''' Parse and convert Markdown headings to HTML '''
     heading_num = len(line) - len(line.lstrip('#'))
     if 1 <= heading_num <= 6:
-        return f'<h{heading_num}>{line.lstrip("#").strip()}</h{heading_num}>\n'
-    return line
+        return f'<h{heading_num}>{line.lstrip("#").strip()}</h{heading_num}>\n', True
+    return line, in_heading
 
 def unordered_list(line, in_unordered_list=False):
     ''' Parse and convert Markdown unordered lists to HTML '''
@@ -43,14 +41,23 @@ def ordered_list(line, in_ordered_list=False):
     else:
         return line, in_ordered_list
 
+def paragraph(line, in_heading=False, in_unordered_list=False, in_ordered_list=False):
+    ''' Parse and convert Markdown paragraphs to HTML '''
+    if not in_unordered_list and not in_ordered_list and not in_heading:
+        if len(line.strip()) > 0:
+            return f'<p>{line.strip()}</p>\n'
+    return line
+
 def convert_markdown_to_html(markdown_file, html_file):
     in_unordered_list = False
     in_ordered_list = False
     with open(markdown_file) as md, open(html_file, 'w') as html:
         for line in md:
+            in_heading = False
             line, in_unordered_list = unordered_list(line, in_unordered_list)
-            line = heading(line)
             line, in_ordered_list = ordered_list(line, in_ordered_list)
+            line, in_heading = heading(line, in_heading)
+            line = paragraph(line, in_unordered_list, in_ordered_list, in_heading)
             html.write(line)
 
 def check_arguments():
